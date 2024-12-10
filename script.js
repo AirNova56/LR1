ac50371316f79f8c8bf3de4914b1d854e0fd87b3
AirNova56 <100962095+AirNova56@users.noreply.github.com>
2024-12-10 22:47:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const x = parseInt(prompt("Input x"));
+
+const a1 = x * x;
+const b1 = a1 * a1;
+const c1 = b1 * b1;
+console.log(c1);
+
+const a2 = x * x;
+const b2 = a2 * a2;
+const c2 = b2 * b2;
+const d2 = c2 * b2;
+const e2 = d2 * c2;
+console.log(e2);
+
+const a3 = x * x;
+const b3 = a3 * a3;
+const c3 = b3 * a3;
+const d3 = c3 * b3;
+console.log(d3);
+
+const a4 = x * x;
+const b4 = a4 * a4;
+const c4 = b4 * b4;
+const d4 = c4 * a4;
+const e4 = d4 * b4;
+console.log(e4); 
+
+const a5 = x * x;
+const b5 = a5 * a5;
+const c5 = b5 * b5;
+const d5 = c5 * b5;
+const e5 = d5 * c5;
+const f5 = e5 * x;
+console.log(f5);
+
+const a6 = x * x;
+const b6 = a6 * a6;
+const c6 = b6 * b6;
+const d6 = c6 * b6;
+const e6 = d6 * a6;
+console.log(e6); 
+
+const a7 = x * x;
+const b7 = a7 * a7;
+const c7 = b7 * b7;
+const d7 = c7 * c7;
+const e7 = d7 * c7;
+const f7 = e7 * b7;
+console.log(f7); 
+
+const a8 = x * x;
+const b8 = a8 * a8;
+const c8 = b8 * b8;
+const d8 = c8 * c8;
+const e8 = d8 * d8;
+const f8 = e8 * e8;
+console.log(f8); 
+
+const a9 = x * x;
+const b9 = a9 * a9;
+const c9 = b9 * b9;
+const d9 = c9 * b9;
+const e9 = d9 * c9;
+console.log(e9);
